ad880405d626e9d86f8eb623507a300eaeab62de
Roman Kozko <romanka2001@mail.ru>
2021-02-16 23:31:42 +0300

    add figure interface

---
+++ Model/FigureInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintOOP.Model
+{
+    struct LineConfiguration
+    {
+        int width;
+        System.Drawing.Color color;
+        System.Drawing.Drawing2D.DashStyle style;
+    }
+    interface FigureI
+    {
+        void drawFigure(System.Windows.Forms.PaintEventArgs e);
+        LineConfiguration pen { get; set; }
+        bool isClosed { get; set; }
+    }
+}
