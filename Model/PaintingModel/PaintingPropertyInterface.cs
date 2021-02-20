1ed9977cf69083ad0ec1d4946c53519b1dd5e910
Roman Kozko <romanka2001@mail.ru>
2021-02-20 18:19:34 +0300

    add painting property interface

---
+++ Model/PaintingModel/PaintingPropertyInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintOOP.Model.PaintingModel
+{
+
+    public struct LineConfiguration
+    {
+        int width;
+        System.Drawing.Color color;
+        System.Drawing.Drawing2D.DashStyle style;
+    }
+    public struct FillConfiguration
+    {
+        System.Drawing.Color color;
+    }
+
+    interface IPaintingProperty
+    {
+        LineConfiguration penProperty { get; set; }
+        FillConfiguration brushProperty { get; set; }
+    }
+}
