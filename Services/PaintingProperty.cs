f758f050c87dd6e30405df51266b82e59b39b36e
Roman Kozko <romanka2001@mail.ru>
2021-02-20 23:24:07 +0300

    add class with paintingProperty

---
+++ Services/PaintingProperty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PaintOOP.Model.PaintingModel;
+
+namespace PaintOOP.Services
+{
+    public class PaintingProperty : IPaintingProperty
+    {
+        public LineConfiguration penProperty { get; set; }
+        public FillConfiguration brushProperty { get; set; }
+    }
+}
