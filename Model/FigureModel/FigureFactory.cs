ad04ae90ecc93e5caf1fe46732432b1a8008681c
Roman Kozko <romanka2001@mail.ru>
2021-02-20 22:19:29 +0300

    change class accessability

--- Model/FigureModel/FigureFactory.cs
+++ Model/FigureModel/FigureFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PaintOOP.Model
 {
-    abstract class FigureFactory
+    public abstract class FigureFactory
     {
         public abstract IFigure create();
     }
