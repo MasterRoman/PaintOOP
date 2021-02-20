07b0c4a14fc9058900485f3d2904548d7006f65a
Roman Kozko <romanka2001@mail.ru>
2021-02-20 22:26:19 +0300

    add parameters

--- Model/FigureModel/FigureFactory.cs
+++ Model/FigureModel/FigureFactory.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PaintOOP.Model.PaintingModel;
 
 namespace PaintOOP.Model
 {
     public abstract class FigureFactory
     {
-        public abstract IFigure create();
+        public abstract IFigure create(System.Drawing.Point points, IPaintingProperty paintingProperty);
     }
 }
