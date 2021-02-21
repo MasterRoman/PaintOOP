24435ade00515dc758b391d861ea71cc78df2557
Roman Kozko <romanka2001@mail.ru>
2021-02-21 11:54:19 +0300

    add helper static class

---
+++ Services/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PaintOOP.Services
+{
+    public static class Helper
+    {
+        public static Rectangle makeCoordsForDrawing(Point topLeftCoords, Point bottomRightCoords)
+        {
+            int width = Math.Abs(topLeftCoords.X - bottomRightCoords.X);
+            int height = Math.Abs(topLeftCoords.Y - bottomRightCoords.Y);
+            
+            Rectangle rect;
+
+            if (topLeftCoords.X < bottomRightCoords.X)
+            {
+                if (topLeftCoords.Y < bottomRightCoords.Y)
+                {
+                    rect = new Rectangle(topLeftCoords.X, topLeftCoords.Y,width,height);
+                }
+                else
+                {
+                    rect = new Rectangle(topLeftCoords.X, bottomRightCoords.Y, width, height);
+                }
+            }
+            else
+            {
+                if (topLeftCoords.Y < bottomRightCoords.Y)
+                {
+                    rect = new Rectangle(bottomRightCoords.X, topLeftCoords.Y, width, height);
+                }
+                else
+                {
+                    rect = new Rectangle(bottomRightCoords.X, bottomRightCoords.Y, width, height);
+                }
+            }
+            return rect;
+        }
+    }
+}
