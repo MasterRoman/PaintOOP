c590bc26441881b6446750f96239f228ab1dbbbd
Roman Kozko <romanka2001@mail.ru>
2021-02-22 21:41:07 +0300

    add ellipse class and factory

---
+++ Model/FigureModel/ParticularFigure/Ellipse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PaintOOP.Model.PaintingModel;
+using PaintOOP.Services;
+
+namespace PaintOOP.Model.FigureModel.ParticularFigure
+{
+    class Ellipse : ClosedFigure
+    {
+
+        public Ellipse(System.Drawing.Point points, IPaintingProperty paintingProperty)
+        {
+
+            this.pen = paintingProperty.penProperty;
+            this.brush = paintingProperty.brushProperty;
+
+            this.topLeftCoords = points;
+            this.bottomRightCoords = points;
+
+        }
+        public override void drawFigure(PaintEventArgs e)
+        {
+            base.drawFigure(e);
+
+            System.Drawing.Pen pen = new System.Drawing.Pen(this.pen.color);
+            pen.Width = this.pen.width;
+            pen.DashStyle = this.pen.style;
+
+            System.Drawing.Brush brush = new System.Drawing.SolidBrush(this.brush.color);
+
+            System.Drawing.Rectangle rect = Helper.makeCoordsForDrawing(this.topLeftCoords, this.bottomRightCoords);
+
+            e.Graphics.FillEllipse(brush, rect);
+
+            e.Graphics.DrawEllipse(pen, rect);
+
+        }
+    }
+
+
+    public class EllipseFactory : FigureFactory
+    {
+        public override IFigure create(System.Drawing.Point points, IPaintingProperty paintingProperty)
+        {
+            return new Ellipse(points, paintingProperty);
+        }
+    }
+}
